0e3663197b1ea0c905b18c0872b6547f062f385f
Cristobal Velasquez Valdes <cristobal.velasquez@ug.uchile.cl>
2019-01-13 23:09:23 -0300

    simple register request
    register api

---
+++ client/src/components/Register.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input type="email" name="email" placeholder="Email" v-model="email">
+    <br>
+    <input type="password" name="password" placeholder="Password" v-model="password">
+    <br>
+    <button @click="sendRegisterData">Register</button>
+
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/authenticationService'
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  methods: {
+    async sendRegisterData () {
+      const response = await AuthenticationService.register({ email: this.email, password: this.password })
+      console.log(response.data)
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email value has changed', value)
+    }
+  }
+}
+</script>
--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,8 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
-  res.send({ messege: 'hello world' })
+app.post('/register', (req, res) => {
+  res.send({ messege: `hello ${req.body.email} your user was registered` })
 })
+
 app.listen(process.env.PORT || 8081)
